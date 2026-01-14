e53d51b49c149ce5594932d52ac014abff72747b
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-01-14 23:19:38 +0530

    Modified number pattern

--- My_Workspace/Host/Youtube/003_Number_1/main.c
+++ My_Workspace/Host/Youtube/003_Number_1/main.c
@@ -10,8 +10,10 @@
 
 int main()
 {
-	int i, j;
-	for (i=1; i<=6; i++)
+	int i, j, rows;
+	printf("Enter the number of rows:");
+	scanf("%d", &rows);
+	for (i=1; i<=rows; i++)
 	{
 		for (j=1; j<=i; j++)
 		{
