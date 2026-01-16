7a499cb637b10c65a8738a55c4ea7e23ff1712d0
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-01-16 23:19:17 +0530

    Youtube star printing patterns

---
+++ My_Workspace/Host/Youtube/006_Star_3/main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ *  Created on: 16-Jan-2026
+ *      Author: CITI
+ */
+
+
+#include<stdio.h>
+
+
+int main(void)
+{
+	int i,j,rows;
+	printf("Enter the number of rows:");
+	scanf("%d", &rows);
+
+	for (i=1; i<=rows; i++)
+	{
+		for (j=1; j<=rows; j++)
+		{
+			if(i+j<=rows)
+			{
+				printf(" ");
+			}
+			else{printf("*");}
+		}
+		printf("\n");
+	}
+
+	getchar();
+	getchar();
+
+}
