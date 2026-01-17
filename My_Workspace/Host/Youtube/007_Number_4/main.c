3ccf385937c84f28d416dac34a9d8930fe11d632
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-01-17 21:22:13 +0530

    Number Pattern program

---
+++ My_Workspace/Host/Youtube/007_Number_4/main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: 17-Jan-2026
+ *      Author: CITI
+ */
+
+
+#include<stdio.h>
+
+
+
+int main (void)
+{
+	int i,j,rows;
+	printf("Enter the number of rows:");
+	scanf("%d", &rows);
+
+	for (i=1; i<=rows; i++)
+	{
+		for (j=1; j<=i; j++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+
+	getchar();
+	getchar();
+}
