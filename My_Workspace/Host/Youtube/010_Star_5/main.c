ed4d5746ded834b9ca4f578b41694f3606e241d5
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-01-20 09:29:24 +0530

    Star pyramid - odd

---
+++ My_Workspace/Host/Youtube/010_Star_5/main.c
@@ -0,0 +1,45 @@
+/*
+ * main.c
+ *
+ *  Created on: 20-Jan-2026
+ *      Author: CITI
+ */
+
+
+#include <stdio.h>
+
+int main (void)
+{
+	int i,j,space,rows;
+
+	printf("Enter the number of rows:");
+	scanf("%d", &rows);
+
+	for (i=1; i<=rows; i++)
+	{
+		for (space =1; space <= (rows -i); space++)
+		{
+			printf("  ");
+
+		}
+
+		for (j=1; j<= (2*i-1); j++)
+		{
+			printf("* ");
+		}
+
+		printf("\n");
+	}
+	getchar();
+	getchar();
+}
+
+//Output
+//Enter the number of rows:7
+//            *
+//          * * *
+//        * * * * *
+//      * * * * * * *
+//    * * * * * * * * *
+//  * * * * * * * * * * *
+//* * * * * * * * * * * * *
