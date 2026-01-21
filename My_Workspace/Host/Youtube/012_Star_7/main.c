ee6c08d315beb34f6f8707cce19fcdd613e3e470
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-01-21 07:49:09 +0530

    Star Pyramid up/down

---
+++ My_Workspace/Host/Youtube/012_Star_7/main.c
@@ -0,0 +1,61 @@
+/*
+ * main.c
+ *
+ *  Created on: 21-Jan-2026
+ *      Author: CITI
+ */
+
+
+#include<stdio.h>
+
+int main (void)
+{
+    int i,j,space,rows;
+
+    printf("Enter the number of rows:");
+    scanf("%d", &rows);
+
+    for(i=1; i<=rows; i++)
+    {
+        for (space =1; space<=(rows-i); space++)
+        {
+            printf(" ");
+
+        }
+
+        for(j=1; j<=(2*i-1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (i=rows-1; i>=1; i--)
+    {
+        for(space =1; space<=(rows-i); space++)
+        {
+            printf(" ");
+        }
+
+        for (j=1; j<=(2*i-1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    getchar();
+    getchar();
+}
+
+//Output
+//Enter the number of rows:5
+//    *
+//   ***
+//  *****
+// *******
+//*********
+// *******
+//  *****
+//   ***
+//    *
