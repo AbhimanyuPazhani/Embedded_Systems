9d94574d4752197e167d42da0b345ffd30d47524
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-04-09 08:39:32 +0530

    Task scheduler

--- My_Workspace/Target/109 Task Scheduler/Src/main.c
+++ My_Workspace/Target/109 Task Scheduler/Src/main.c
@@ -18,39 +18,31 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include "main.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   //#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 void init_systick_timer(uint32_t tick_hz);
 
+void init_tasks_stack(void);
 __attribute__((naked)) void init_scheduler_stack(uint32_t sched_top_of_stack);
 void task1_handler (void); //Task 1
 void task2_handler (void); //Task 2
 void task3_handler (void); //Task 3
 void task4_handler (void); //Task 4
 
-//some stack memory calculations
-#define  SIZE_TASK_STACK		 1024U
-#define  SIZE_SCHED_STACK		 1024U
+uint32_t psp_of_tasks[MAX_TASKS] = {T1_STACK_START, T2_STACK_START, T3_STACK_START, T4_STACK_START};
 
-#define SRAM_START  	0X2000000U
-#define SIZE_SRAM		((128)*(1024))
-#define SRAM_END	((SRAM_START) + (SIZE_SRAM))
-
-#define T1_STACK_START 		SRAM_END
-#define T2_STACK_START 		((SRAM_END) - (1*SIZE_TASK_STACK))
-#define T3_STACK_START		((SRAM_END) - (2*SIZE_TASK_STACK))
-#define T4_STACK_START		((SRAM_END) - (3*SIZE_TASK_STACK))
-#define SCHED_STACK_START 	((SRAM_END) - (4*SIZE_TASK_STACK))
-
-#define TICK_HZ 			1000U
-#define HSI_CLOCK 			16000000U
-#define SYSTICK_TIM_CLK 	HSI_CLOCK
-
+uint32_t task_handlers [MAX_TASKS];
 int main(void)
 {
 	init_scheduler_stack(SCHED_STACK_START);
+	task_handlers[0] = (uint32_t) task1_handler;
+	task_handlers[1] = (uint32_t) task2_handler;
+	task_handlers[2] = (uint32_t) task3_handler;
+	task_handlers[3] = (uint32_t) task4_handler;
+	init_tasks_stack();
 	init_systick_timer(TICK_HZ);
     /* Loop forever */
 	for(;;);
@@ -115,6 +107,32 @@ __attribute__((naked)) void init_scheduler_stack(uint32_t sched_top_of_stack)
 
 }
 
+
+void init_tasks_stack(void)
+{
+
+	uint32_t *pPSP;
+ for (int i=0; i<MAX_TASKS; i++)
+ {
+	 pPSP = (uint32_t *)psp_of_tasks[i];
+	 pPSP--;
+	 *pPSP = DUMMY_XPSR; //0X01000000
+
+	 pPSP--; //PC
+	 *pPSP = task_handlers[i];
+
+	 pPSP--; //LR
+	 *pPSP = 0xFFFFFFFD;
+
+
+	 for(int j=0; j<13; j++)
+	 {
+		 pPSP--;
+			 *pPSP = 0; //0X01000000
+	 }
+	 psp_of_tasks[i] = (uint32_t) pPSP;
+ }
+}
 void SysTick_Handler (void)
 {
 
