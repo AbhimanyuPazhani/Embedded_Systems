2f5888d6a5d03fa71f5687b2d5bc536fd1b47689
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-04-11 08:42:18 +0530

    Task scheduler completed

--- My_Workspace/Target/109 Task Scheduler/Src/main.c
+++ My_Workspace/Target/109 Task Scheduler/Src/main.c
@@ -142,9 +142,36 @@ void init_tasks_stack(void)
 	 }
 	 psp_of_tasks[i] = (uint32_t) pPSP;
  }
+
+
 }
-void SysTick_Handler (void)
+__attribute__((naked)) void SysTick_Handler (void)
 {
+/* save the context of the current task*/
+
+	//1. Get current running task's psp value.
+	__asm volatile ("MRS R0, PSP");
+	//2. Using PSP value store SF2 (R4 to R11)
+	__asm volatile  ("STMDB R0!, {R4-R11}");
+	//3. Save the current value of the PSP
+	__asm volatile ("PUSH {LR}");
+	__asm volatile ("BL save_psp_value");
+
+	/* Retrieve the context of the next task*/
+
+	//1. Decide next task to run.
+	__asm volatile ("BL update_next_task");
+	//2.Get its past PSP value
+	__asm volatile ("BL get_psp_value");
+	//3. Using that PSP value retrieve SF2 (R4 to R11).
+	__asm volatile ("LDMIA R0!, {R4-R11}");
+	//4. Update PSP and exit.
+	__asm volatile ("MSR PSP, R0");
+	__asm volatile ("POP {LR}");
+
+	__asm volatile ("BX LR");
+
+
 
 }
 
@@ -153,6 +180,18 @@ uint32_t get_psp_value(void)
 {
 	return psp_of_tasks[current_task];
 }
+
+void save_psp_value (uint32_t current_psp_value)
+{
+	psp_of_tasks[current_task] = current_psp_value;
+}
+
+void update_next_task (void)
+{
+	current_task++;
+	current_task = current_task % MAX_TASKS;
+}
+
 
 __attribute__((naked)) void switch_sp_to_psp(void)
 {
