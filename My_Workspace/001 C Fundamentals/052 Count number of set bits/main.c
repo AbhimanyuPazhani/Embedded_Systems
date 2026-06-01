d41713e9cceabf60d03483d9b2dc045dee81c7ce
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-06-01 20:10:29 +0530

    Count number of set bits

--- My_Workspace/001 C Fundamentals/052 Count number of set bits/main.c
+++ My_Workspace/001 C Fundamentals/052 Count number of set bits/main.c
@@ -7,6 +7,14 @@
 
 #include<stdio.h>
 
+void wait_for_input (void)
+{
+	printf("\nPress enter to EXIT");
+	while (getchar()!= '\n');
+	getchar();
+}
+
+#if count
 int countsetbits(unsigned int num)
 {
 	int count =0;
@@ -19,12 +27,7 @@ int countsetbits(unsigned int num)
 
 	return count;
 }
-void wait_for_input (void)
-{
-	printf("\nPress enter to EXIT");
-	while (getchar()!= '\n');
-	getchar();
-}
+
 
 int main()
 {
@@ -34,4 +37,32 @@ int main()
 
 	printf("\nSet bits = %d", countsetbits(num));
 	wait_for_input ();
+}
+
+#endif
+
+//Method 2: Brian Kernighan's Algorithm
+
+int countSetBits(unsigned int num)
+{
+    int count = 0;
+
+    while(num)
+    {
+        num = num & (num - 1);
+        count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+	unsigned int num;
+	printf("Enter the number:");
+	scanf("%d", &num);
+
+    printf("Number of set bits = %d\n", countSetBits(num));
+
+    wait_for_input ();
 }
